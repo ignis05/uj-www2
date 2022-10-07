01257c50a329f3a930dd3123816b27652ddee61b
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-10-07 14:37:49 +0200

    feat: register page

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms'
 
 @Component({
 	selector: 'app-register',
@@ -6,7 +7,37 @@ import { Component, OnInit } from '@angular/core'
 	styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-	constructor() {}
+	registerForm: FormGroup
+	constructor() {
+		this.registerForm = new FormGroup({
+			login: new FormControl('', [Validators.required, Validators.minLength(4)]),
+			password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+			password2: new FormControl('', [Validators.required, Validators.minLength(8), this.passwordsValidator]),
+		})
+	}
 
 	ngOnInit(): void {}
+
+	passwordsValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+		let group = control.parent
+		if (!group) return null
+		let pass = group.get('password')?.value
+		let pass2 = group.get('password2')?.value
+		return pass === pass2 ? null : { notSame: true }
+	}
+
+	getErrorMessage(control: AbstractControl | null) {
+		if (!control) return ''
+		if (control.hasError('required')) return 'You must enter a value'
+		if (control.hasError('minlength')) return 'Value too short'
+		if (control.hasError('notSame')) return 'Passwords are not the same'
+
+		return 'unspecified error'
+	}
+
+	register(): void {
+		let login = this.registerForm.get('login')?.value
+		let password = this.registerForm.get('password')?.value
+		console.log(login, password)
+	}
 }
