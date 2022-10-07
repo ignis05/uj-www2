9e5ceb8013e9302083bdbde97b3028ac173f1c21
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-10-07 11:46:01 +0200

    feat: login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { ReactiveFormsModule } from '@angular/forms'
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatButtonModule } from '@angular/material/button'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module'
@@ -8,7 +15,16 @@ import { LoginComponent } from './components/login/login.component'
 
 @NgModule({
 	declarations: [AppComponent, RegisterComponent, LoginComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		NgbModule,
+		BrowserAnimationsModule,
+		ReactiveFormsModule,
+		MatInputModule,
+		MatFormFieldModule,
+		MatButtonModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { FormControl } from '@angular/forms'
 
 @Component({
 	selector: 'app-login',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core'
 	styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+	login = new FormControl('')
+	password = new FormControl('')
+
 	constructor() {}
 
 	ngOnInit(): void {}
+
+	signIn(): void {
+		let login = this.login.value
+		let password = this.login.value
+		console.log(login, password)
+	}
 }
