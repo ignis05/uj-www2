f7a8ae99912dc3191efa50f103ed11828e8e1ba7
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-10-07 10:58:58 +0200

    feat: router app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'uj-www2';
+	title = 'uj-www2'
 }
